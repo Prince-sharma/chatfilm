32d6d8b973ec6a864070115071df9ce2e94b2f5d
prince-sharma <princesharma.pri@gmail.com>
2025-05-01 12:27:51 +0530

    changed the blue bar color

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Additional PWA meta tags for iOS */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="theme-color" content="#111827" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
